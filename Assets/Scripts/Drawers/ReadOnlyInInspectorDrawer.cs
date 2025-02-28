8863908bb81f936b467cc54c0b84c7b0ee547e62
22520565 <22520565@gm.uit.edu.vn>
2025-02-28 17:09:08 +0700

    Add comment for `ReadOnlyInInspectorAttribute`. Enhance `ReadOnlyInInspectorDrawer`.

--- Assets/Scripts/Drawers/ReadOnlyInInspectorDrawer.cs
+++ Assets/Scripts/Drawers/ReadOnlyInInspectorDrawer.cs
@@ -11,7 +11,7 @@ public sealed class ReadOnlyInInspectorDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label);
+        EditorGUI.PropertyField(position, property, label, true);
         GUI.enabled = true;
     }
 }
