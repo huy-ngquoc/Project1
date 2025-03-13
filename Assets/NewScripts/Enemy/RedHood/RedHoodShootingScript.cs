8da4903973a7bef9e7bd9ee925087132f0aac726
BinhUIT <22520128@gm.uit.edu.vn>
2025-03-13 07:44:05 +0700

    Refector shoot for red hood

--- Assets/NewScripts/Enemy/RedHood/RedHoodShootingScript.cs
+++ Assets/NewScripts/Enemy/RedHood/RedHoodShootingScript.cs
@@ -7,8 +7,16 @@ namespace Game
         [SerializeField] RedHood redHood;
         protected  void OnTriggerEnter2D(Collider2D col) {
             if(col.gameObject.tag=="Player") {
-                redHood.ChangeState(new ShootingState(redHood));
+                redHood.ChangeState(new ShootingState(redHood)); 
+                Invoke("SpawnArrow",0.5f);
             }
-        }
+        } 
+    public virtual void SpawnArrow(){
+        GameObject arrow = redHood.GetArrow();
+        Vector3 arrowRotation=new Vector3(0,180-this.redHood.GetDirection(),0);
+        Vector3 arrowPosition = this.redHood.GetTransform().position+new Vector3(0,-1,0);
+        Instantiate(arrow, arrowPosition, Quaternion.Euler(arrowRotation));
     }
+    }
+   
 }
