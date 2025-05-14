ea0120193cec37ad6778083abfbdd4ba7b7e906d
BinhUIT <22520128@gm.uit.edu.vn>
2025-05-14 21:06:34 +0700

    Them am thanh

--- Assets/BinhScript/UIScript/MySceneManager.cs
+++ Assets/BinhScript/UIScript/MySceneManager.cs
@@ -7,23 +7,41 @@ namespace Game
     public class MySceneManager : MonoBehaviour
     {
        public void PressPlayButton() {
-        SceneManager.LoadScene(2);
+          AudioManager.instance.PlayPressButtonSound();
+          SceneManager.LoadScene(2);
        }
        public void SetEasy() {
+            AudioManager.instance.PlayPressButtonSound();
             PlayerPrefs.SetInt("Difficulty",0); //0:easy
             SceneManager.LoadScene(3);
        }
        public void SetHard() {
+            AudioManager.instance.PlayPressButtonSound();
             PlayerPrefs.SetInt("Difficulty",1) ;//1:hard
             SceneManager.LoadScene(3);
 
        }
        public void BackToHome() {
+            AudioManager.instance.PlayPressButtonSound();
             SceneManager.LoadScene(0);
        }
        public void BackToDifficulty() {
+            AudioManager.instance.PlayPressButtonSound();
             SceneManager.LoadScene(2);
        } 
+       public void SelectLevel1() {
+          Debug.Log("Load level1");
+          SceneManager.LoadScene(4);
+       }
+       public void SelectLevel2() {
+
+       } 
+       public void SelectLevel3() {
+
+       } 
+       public void SelectLevel4() {
+
+       }
 
     }
 }
